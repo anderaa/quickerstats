770691155cd94ddc1412ca821f70bbdf8bc37a41
anderaa <aaronanderson.co@gmail.com>
2019-10-08 15:08:17 -0600

    added tests for get_state_data

--- tests/testthat/test_quickerstats.R
+++ tests/testthat/test_quickerstats.R
@@ -3,13 +3,33 @@ context("Structure of objects returned")
 
 key <- Sys.getenv('NASS_KEY')
 
-
 test_that("get_param_values returns objects of correct size and type", {
-  r <- get_param_values(key, "short_desc")
+  r <- get_param_values(key=key, param="short_desc")
   expect_equal(class(r), 'list')
   expect_equal(class(r[[1]]), 'list')
   expect_equal(class(r[[1]][[1]]), 'character')
   expect_equal(length(r), 1)
   expect_true(length(r[[1]]) > 0)
   expect_equal(length(r[[1]][[1]]), 1)
+})
+
+test_that("search_data_items returns objects of correct size and type", {
+  r <- search_data_items(key=key, search_terms=c('corn'))
+  expect_equal(class(r), 'character')
+  expect_equal(class(r[1]), 'character')
+  expect_equal(length(r[1]), 1)
+  expect_true(length(r) > 0)
+})
+
+test_that("search_state_data returns objects of correct size and type", {
+  r1 <- get_state_data(key=key, year=2017,
+                       data_item='CORN, GRAIN - ACRES HARVESTED', fips='all')
+  r2 <- get_state_data(key=key, year=2017,
+                       data_item='CORN, GRAIN - ACRES HARVESTED', fips='08')
+  expect_equal(class(r1)[1], 'spec_tbl_df')
+  expect_true(nrow(r1) > 20 & nrow(r1) < 55)
+  expect_true(ncol(r1) > 30)
+  expect_equal(class(r2)[1], 'spec_tbl_df')
+  expect_equal(nrow(r2), 1)
+  expect_true(ncol(r2) > 30)
 })
