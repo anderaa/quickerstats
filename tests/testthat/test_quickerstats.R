d26d316795c6378c0d949c7b9b764ad11f07f069
anderaa <aaronanderson.co@gmail.com>
2019-10-08 15:36:04 -0600

    add tests for get_county_data

--- tests/testthat/test_quickerstats.R
+++ tests/testthat/test_quickerstats.R
@@ -1,5 +1,5 @@
 
-context("Structure of objects returned")
+context("Structure of objects returned from valid queries")
 
 key <- Sys.getenv('NASS_KEY')
 
@@ -21,7 +21,7 @@ test_that("search_data_items returns objects of correct size and type", {
   expect_true(length(r) > 0)
 })
 
-test_that("search_state_data returns objects of correct size and type", {
+test_that("get_state_data returns objects of correct size and type", {
   r1 <- get_state_data(key=key, year=2017,
                        data_item='CORN, GRAIN - ACRES HARVESTED', fips='all')
   r2 <- get_state_data(key=key, year=2017,
@@ -32,4 +32,22 @@ test_that("search_state_data returns objects of correct size and type", {
   expect_equal(class(r2)[1], 'spec_tbl_df')
   expect_equal(nrow(r2), 1)
   expect_true(ncol(r2) > 30)
+})
+
+test_that("get_county_data returns objects of correct size and type", {
+  r1 <- get_county_data(key=key, year=2017,
+                        data_item='CORN, GRAIN - ACRES HARVESTED', fips='all')
+  r2 <- get_county_data(key=key, year=2017,
+                        data_item='CORN, GRAIN - ACRES HARVESTED', fips='08')
+  r3 <- get_county_data(key=key, year=2017,
+                        data_item='CORN, GRAIN - ACRES HARVESTED', fips='08069')
+  expect_equal(class(r1)[1], 'spec_tbl_df')
+  expect_true(nrow(r1) > 500 & nrow(r1) < 3800)
+  expect_true(ncol(r1) > 30)
+  expect_equal(class(r2)[1], 'spec_tbl_df')
+  expect_true(nrow(r2) > 10)
+  expect_true(ncol(r2) > 30)
+  expect_equal(class(r3)[1], 'spec_tbl_df')
+  expect_equal(nrow(r3), 1)
+  expect_true(ncol(r3) > 30)
 })
