751356cc38917ba9871abf54a1ef62a7f1f497bd
anderaa <aaronanderson.co@gmail.com>
2019-10-10 06:48:15 -0600

    add tests for bad keys

--- tests/testthat/test_quickerstats.R
+++ tests/testthat/test_quickerstats.R
@@ -7,6 +7,20 @@ skip_if_no_auth <- function() {
   }
 }
 
+test_that("check for error with bad key", {
+  expect_error(get_param_values(key='bad_key', param='short_desc'))
+  expect_error(get_options(key='bad_key',
+               data_item='CORN, GRAIN - ACRES HARVESTED'))
+  expect_error(search_data_items(key='bad_key',
+                                 search_terms=c('corn')))
+  expect_error(get_state_data(key='bad_key', year=2017,
+               data_item='CORN, GRAIN - ACRES HARVESTED',
+               fips='all'))
+  expect_error(get_county_data(key='bad_key', year=2017,
+                               data_item='CORN, GRAIN - ACRES HARVESTED',
+                               fips='all'))
+})
+
 test_that("get_param_values returns objects of correct size and type", {
   skip_if_no_auth()
   r <- get_param_values(key=Sys.getenv('NASS_KEY'), param='short_desc')
