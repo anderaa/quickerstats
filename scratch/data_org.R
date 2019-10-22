b4310d6456c85dce36912537675f224efa730d3f
anderaa <aaronanderson.co@gmail.com>
2019-10-22 06:30:58 -0600

    finished both state and county datasets

--- scratch/data_org.R
+++ scratch/data_org.R
@@ -2,7 +2,7 @@
 # This script creates the data sets written to R/sysdata.rda
 
 # organize and write state data
-state_fips <- read.csv('scratch/state_fips.csv')
+state_fips <- read.csv('scratch/state_fips.csv', stringsAsFactors = FALSE)
 state_fips['state_fips_string'] <- NA
 for (i in 1:nrow(state_fips)) {
   if (state_fips[i, 'state_fips'] < 10) {
@@ -13,6 +13,38 @@ for (i in 1:nrow(state_fips)) {
 }
 state_fips <- state_fips[c('name', 'state_fips_string')]
 colnames(state_fips) <- c('state', 'state_fips')
-usethis::use_data(state_fips, internal = TRUE)
+usethis::use_data(county_fips, internal = TRUE, overwrite = TRUE)
+
 
 # organize and write county data
+county_fips <- read.csv('scratch/county_fips.csv', stringsAsFactors = FALSE)
+county_fips['county_fips'] <- NA
+county_fips['county_code_string'] <- NA
+county_fips['state_fips_string'] <- NA
+county_fips['state'] <- NA
+for (i in 1:nrow(county_fips)) {
+  # fix state code string
+  if (county_fips[i, 'state_fips'] < 10) {
+    county_fips[i, 'state_fips_string'] <- paste('0', as.character(county_fips[i, 'state_fips']), sep='')
+  } else {
+    county_fips[i, 'state_fips_string'] <- as.character(county_fips[i, 'state_fips'])
+  }
+  # fix county code string
+  if (county_fips[i, 'county_code'] < 10) {
+    county_fips[i, 'county_code_string'] <- paste('00', as.character(county_fips[i, 'county_code']), sep='')
+  } else if ((county_fips[i, 'county_code'] < 100)) {
+    county_fips[i, 'county_code_string'] <- paste('0', as.character(county_fips[i, 'county_code']), sep='')
+  } else {
+    county_fips[i, 'county_code_string'] <- as.character(county_fips[i, 'county_code'])
+  }
+  # paste state and county together
+  county_fips[i, 'county_fips'] <- paste(county_fips[i, 'state_fips_string'],
+                                         county_fips[i, 'county_code_string'],
+                                         sep='')
+  # add in state names
+  county_fips[i, 'state'] <- state_fips[state_fips$state_fips == county_fips[i, 'state_fips_string'], 'state']
+
+}
+county_fips <- county_fips[c('name', 'state', 'county_fips')]
+colnames(county_fips) <- c('county', 'state', 'county_fips')
+usethis::use_data(state_fips, internal = TRUE, overwrite=TRUE)
